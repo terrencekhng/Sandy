97c3d38df8de01c986f1ad72f96cc898c6bba15b
Terence Ng <awesome.terence@gmail.com>
2014-08-22 22:07:35 +0800

    provide grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2014/8/21.
+ */
+module.exports = function (grunt) {
+
+  //Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  });
+}
